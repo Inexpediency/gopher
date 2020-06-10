dcd3988f69c38e5ac631fe84bf7d7d84e4514595
Ythosa <vasus714@yandex.ru>
2020-06-10 07:12:05 +0300

    topology sort

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	types.TestSquares()
+	types.TestTopologySort()
 }
--- types/funcs.go
+++ types/funcs.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"golang.org/x/net/html"
 	"os"
+	"sort"
 )
 
 func TestForEachNode() {
@@ -69,3 +70,49 @@ func TestSquares() {
 	fmt.Println(f()) // ...
 	fmt.Println(f())
 }
+
+func TestTopologySort() {
+	var prereqs = map[string][]string{
+		"algorithms": {"data structures"},
+		"calculus": {"linear algebra"},
+		"compilers" : {"data structures", "formal languages", "computer organization"},
+		"data structures" : {"discrete math"},
+		"databases": {"data structures"},
+		"discrete math": {"intro to programming"},
+		"formal languages": {"discrete math"},
+		"networks": {"operating systems"},
+		"programming languages": {"data structures", "computer organization"},
+	}
+
+	for i, course := range TopologySort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i + 1, course)
+	}
+}
+
+func TopologySort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+
+	var visitAll func(items []string)
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+
+				order = append(order, item)
+			}
+		}
+	}
+
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+	visitAll(keys)
+
+	return order
+}
+
