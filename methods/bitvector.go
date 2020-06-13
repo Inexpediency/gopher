3fc11200a0e405550f177570db778e776b6ad116
Ythosa <vasus714@yandex.ru>
2020-06-13 14:13:52 +0300

    bit vector

---
+++ methods/bitvector.go
@@ -0,0 +1,57 @@
+package methods
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type IntSet struct {
+	words []uint64
+}
+
+// Has specifies whether the set contains a non-negative value of X.
+func (s *IntSet) Has (x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add adds a non negative value x to the set
+func (s *IntSet) Add (x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith makes the set s equal to the Union of sets s and t
+func (s *IntSet) UnionWith (t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// String returns a set as a string of the form " {1 2 3}"
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64 ; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+			}
+			fmt.Fprintf(&buf, "%d", 64*i+j)
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
