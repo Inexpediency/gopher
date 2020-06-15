44d3ed66e0a837dc94f761b6e46ffb5b58c6ec85
Ythosa <vasus714@yandex.ru>
2020-06-15 12:58:32 +0300

    test counter errors

--- counter/counter_test.go
+++ counter/counter_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 )
 
+//!+Eval
 func TestEval(t *testing.T) {
 	tests := []struct {
 		expr string
@@ -19,26 +20,86 @@ func TestEval(t *testing.T) {
 		{"5 / 9 * (F - 32)", counter.Env{"F": -40}, "-40"},
 		{"5 / 9 * (F - 32)", counter.Env{"F": 32}, "0"},
 		{"5 / 9 * (F - 32)", counter.Env{"F": 212}, "100"},
+		//!-Eval
+		// additional tests that don't appear in the book
+		{"-1 + -x", counter.Env{"x": 1}, "-2"},
+		{"-1 - x", counter.Env{"x": 1}, "-2"},
+		//!+Eval
 	}
-
 	var prevExpr string
 	for _, test := range tests {
-		// Return expr, only where it is changed
+		// Print expr only when it changes.
 		if test.expr != prevExpr {
 			fmt.Printf("\n%s\n", test.expr)
 			prevExpr = test.expr
 		}
-
 		expr, err := counter.Parse(test.expr)
 		if err != nil {
-			t.Error(err) // Analyze error
+			t.Error(err) // parse error
 			continue
 		}
-			got := fmt.Sprintf("%.6g", expr.Eval(test.env))
-			fmt.Printf("\t%v => %s\n", test.env, got)
-			if got != test.want{
-				t.Errorf("%s.Eval() в %v = %q, must be %q\n",
-					test.expr, test.env, got, test.want)
+		got := fmt.Sprintf("%.6g", expr.Eval(test.env))
+		fmt.Printf("\t%v => %s\n", test.env, got)
+		if got != test.want {
+			t.Errorf("%s.Eval() in %v = %q, want %q\n",
+				test.expr, test.env, got, test.want)
 		}
 	}
 }
+
+//!-Eval
+
+/*
+//!+output
+sqrt(A / pi)
+	map[A:87616 pi:3.141592653589793] => 167
+pow(x, 3) + pow(y, 3)
+	map[x:12 y:1] => 1729
+	map[x:9 y:10] => 1729
+5 / 9 * (F - 32)
+	map[F:-40] => -40
+	map[F:32] => 0
+	map[F:212] => 100
+//!-output
+// Additional outputs that don't appear in the book.
+-1 - x
+	map[x:1] => -2
+-1 + -x
+	map[x:1] => -2
+*/
+
+func TestErrors(t *testing.T) {
+	for _, test := range []struct{ expr, wantErr string }{
+		{"x % 2", "unexpected '%'"},
+		{"math.Pi", "unexpected '.'"},
+		{"!true", "unexpected '!'"},
+		{`"hello"`, "unexpected '\"'"},
+		{"log(10)", `unknown function "log"`},
+		{"sqrt(1, 2)", "call to sqrt has 2 args, want 1"},
+	} {
+		expr, err := counter.Parse(test.expr)
+		if err == nil {
+			vars := make(map[counter.Var]bool)
+			err = expr.Check(vars)
+			if err == nil {
+				t.Errorf("unexpected success: %s", test.expr)
+				continue
+			}
+		}
+		fmt.Printf("%-20s%v\n", test.expr, err) // (for book)
+		if err.Error() != test.wantErr {
+			t.Errorf("got error %s, want %s", err, test.wantErr)
+		}
+	}
+}
+
+/*
+//!+errors
+x % 2               unexpected '%'
+math.Pi             unexpected '.'
+!true               unexpected '!'
+"hello"             unexpected '"'
+log(10)             unknown function "log"
+sqrt(1, 2)          call to sqrt has 2 args, want 1
+//!-errors
+*/
