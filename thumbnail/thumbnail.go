4fa08b2973ae3b0670beb6159456c7256cd45521
ythosa <vasus714@yandex.ru>
2020-06-20 16:55:11 +0300

    fix

--- thumbnail/thumbnail.go
+++ thumbnail/thumbnail.go
@@ -3,14 +3,11 @@
 // The thumbnail command produces thumbnails of JPEG files
 // whose names are provided on each line of the standard input.
 //
-// The "+build ignore" tag (see p.295) excludes this file from the
-// thumbnail package, but it can be compiled as a command and run like
-// this:
-//
 // Run with:
 //   - Start `Run()` from main.go
-//   - Run program:
-//   	$ make run
+//   - Build program:
+//   	$ make
+//		$ ./main
 //   	$ foo.jpeg
 //   	$ ^D
 //
