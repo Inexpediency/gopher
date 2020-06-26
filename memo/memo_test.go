2b5525f49139feb5f0e2493a553354d1228f7ba6
Ythosa <vasus714@yandex.ru>
2020-06-26 23:05:01 +0300

    tests

---
+++ memo/memo_test.go
@@ -0,0 +1,17 @@
+package memo_test
+
+import "testing"
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	defer m.Close()
+	memotest.Sequential(t, m)
+}
+
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	defer m.Close()
+	memotest.Concurrent(t, m)
+}
