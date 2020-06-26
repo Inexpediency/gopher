da94ccd2bb273b667554b7cbc2d009d66f1bbe29
Ythosa <vasus714@yandex.ru>
2020-06-26 23:13:01 +0300

    refactoring

--- memo/memo_test.go
+++ memo/memo_test.go
@@ -1,17 +1,100 @@
+// package memo_test provides common functions for
+// testing various designs of the memo package.
+
 package memo_test
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"testing"
+	"time"
 
-var httpGetBody = memotest.HTTPGetBody
+	"github.com/ythosa/gobih/memo"
+)
 
 func Test(t *testing.T) {
 	m := memo.New(httpGetBody)
 	defer m.Close()
-	memotest.Sequential(t, m)
+	Sequential(t, m)
 }
 
 func TestConcurrent(t *testing.T) {
 	m := memo.New(httpGetBody)
 	defer m.Close()
-	memotest.Concurrent(t, m)
+	Concurrent(t, m)
+}
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+var HTTPGetBody = httpGetBody
+
+func incomingURLs() <-chan string {
+	ch := make(chan string)
+	go func() {
+		for _, url := range []string{
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"https://github.com",
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"https://github.com",
+			"https://ythosa.github.io",
+		} {
+			ch <- url
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+type M interface {
+	Get(key string) (interface{}, error)
+}
+
+// m := memo.New(httpGetBody)
+
+func Sequential(t *testing.T, m M) {
+	for url := range incomingURLs() {
+		start := time.Now()
+		value, err := m.Get(url)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Printf("%s, %s, %d bytes\n",
+			url, time.Since(start), len(value.([]byte)))
+	}
+}
+
+// m := memo.New(httpGetBody)
+
+func Concurrent(t *testing.T, m M) {
+	var n sync.WaitGroup
+	for url := range incomingURLs() {
+		n.Add(1)
+		go func(url string) {
+			defer n.Done()
+			start := time.Now()
+			value, err := m.Get(url)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			fmt.Printf("%s, %s, %d bytes\n",
+				url, time.Since(start), len(value.([]byte)))
+		}(url)
+	}
+	n.Wait()
 }
